b7290826e8f828f2bba9992ee52a72d9e724dd39
Cristian <shippo658@gmail.com>
2020-02-09 17:41:19 +0200

    Add footer

--- src/js/script.js
+++ src/js/script.js
@@ -5,6 +5,7 @@ $(document).ready(function(){
         nextArrow: '<button type="button" class="slick-next"><img src="../icons/right.svg"></button>'
       });
 
+      //Tabs
       $('ul.catalog__tabs').on('click', 'li:not(.catalog__tab_active)', function() {
         $(this)
           .addClass('catalog__tab_active').siblings().removeClass('catalog__tab_active')
@@ -26,5 +27,20 @@ $(document).ready(function(){
             $('.catalog-item__lists').eq(i).toggleClass('catalog-item__lists_active');
         })
     });
+
+    //Modal
+
+    $('[data-modal=consultation]').on('click', function() {
+        $('.overlay, #consultation').fadeIn('slow'); 
+    });
+    $('.modal__close').on('click', function() {
+        $('.overlay, #consultation, #thanks, #order').fadeOut('slow');       
+    });
+    $('.button_mini').each(function(i){
+        $(this).on('click', function() {
+            $('#order, .modal__descr').text($('.catalog-item__subtitle').eq(i).text());
+            $('.overlay, #order').fadeIn('slow');
+        });
+    });    
 
   });
